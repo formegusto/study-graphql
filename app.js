fd546c3ae7606eb9a5df824edf28f46e32fa2004
formegusto <formegusto@gmail.com>
2021-01-03 03:42:46 +0900

    GraphQLBuildSchema

--- app.js
+++ app.js
@@ -1,34 +1,49 @@
 var express = require('express');
 var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+var Graphql = require('graphql');
 
-var schema = buildSchema(`
-  type Query {
-    hello: String
-    persons: [Person]
-  }
-
-  type Person {
-      name: String
-      age: Int
-  }
-`);
-
-var root = { 
-    hello: () => 'Hello world!', 
-    persons: () => {
-        return [
-            {name: "kim", age: 20},
-            {name: "lee", age: 30},
-            {name: "park", age: 40},
-        ];
+const personType = new Graphql.GraphQLObjectType({
+    name: "Person",
+    fields: {
+        id: { type: Graphql.GraphQLID },
+        name: { type: Graphql.GraphQLString },
+        age: { type: Graphql.GraphQLInt }
     }
-};
+});
+
+const personData = [
+    {id:1000, name: "kim", age: 20},
+    {id:2000, name: "lee", age: 30},
+    {id:3000, name: "park", age: 40},
+];
+
+const queryType = new Graphql.GraphQLObjectType({
+    name: "Query",
+    fields: {
+        hello: {
+            type: Graphql.GraphQLString,
+            resolve: () => 'Hello world',
+        },
+        person: {
+            type: personType,
+            args: {
+                id: {type: Graphql.GraphQLInt}
+            },
+            resolve: (_,args) => 
+                personData.find((person) => person.id === args.id)
+        },
+        persons: {
+            type: new Graphql.GraphQLList(personType),
+            resolve: () => personData
+        }
+    }
+})
+
+const schema = new Graphql.GraphQLSchema({query: queryType});
 
 var app = express();
 app.use('/graphql', graphqlHTTP.graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
+    schema: schema,
+    graphiql: true,
 }));
-app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
+app.listen(4000, () => console.log('Now brows to localhost:4000/graphql'));
