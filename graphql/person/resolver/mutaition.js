956e91d9bd92e6b6a36d494921bc98e2bff28482
formegusto <formegusto@gmail.com>
2021-01-05 23:18:29 +0900

    CRUD Without DB

--- graphql/person/resolver/mutaition.js
+++ graphql/person/resolver/mutaition.js
@@ -1,4 +1,5 @@
 const GraphQL = require('graphql');
+const person = require('..');
 const { personData, personAddress } = require('../data');
 const { PersonType, PersonInputType, PersonAddressType } = require('../type');
 
@@ -16,6 +17,34 @@ const MutationResolver = new GraphQL.GraphQLObjectType({
 
                 return personData;
             },
+        },
+        updatePerson: {
+            type: GraphQL.GraphQLList(PersonType),
+            args: {
+                id: { type: GraphQL.GraphQLID },
+                name: { type: GraphQL.GraphQLString }
+            },
+            resolve: (_, args) => {
+                return personData.map((person) => 
+                   (person.id === args.id) ? 
+                        {
+                            ...person,
+                            name: args.name
+                        }
+                        :
+                        person
+                );
+            }
+        },
+        deletePerson: {
+            type: GraphQL.GraphQLList(PersonType),
+            args: {
+                id: { type: GraphQL.GraphQLID }
+            },
+            resolve: (_,args) => {
+                return personData.filter((person) => 
+                    person.id != args.id);
+            }
         }
     }
 });
